9e07a711ca43c419794daa7fef7ad97fe12e46d5
anmariyafrancis <anmariya1915@gmail.com>
2020-03-02 20:21:05 -0500

    day2 Alert AND BACK BUTTON

---
+++ Day2/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Day2
+//
+//  Created by MacStudent on 2020-03-02.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
